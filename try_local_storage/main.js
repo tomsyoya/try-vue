ea480681290a54b4dccdfae55c82186df7846c67
Soya Sano <soyasano2180plus1@gmail.com>
2020-09-24 21:25:07 +0900

    Add cart function using localStorage.setItem

--- try_local_storage/main.js
+++ try_local_storage/main.js
@@ -3,6 +3,8 @@ const app = new Vue({
   data: {
     name: "",
     age: 0,
+    cats: [],
+    newCat: null,
   },
   mounted() {
     //ページ読み込み時にlocalStorageにパラメータが保存されていたら、formにパラメータを表示
@@ -11,6 +13,13 @@ const app = new Vue({
     }
     if (localStorage.age) {
       this.age = localStorage.age;
+    }
+    if (localStorage.getItem("cats")) {
+      try {
+        this.cats = JSON.parse(localStorage.getItem("cats"));
+      } catch (e) {
+        localStorage.removeItem("cats");
+      }
     }
   },
   //watchを使ったらformに入力された時点でlocalStorageに値が格納される
@@ -24,7 +33,25 @@ const app = new Vue({
     persist() {
       localStorage.name = this.name;
       localStorage.age = this.age;
-      console.log("now pretended I did more stuff....");
+      //   console.log("now pretended I did more stuff....");
+    },
+    addCat() {
+      //formに何も入力されなかったら何もしない
+      if (!this.newCat) {
+        return;
+      }
+
+      this.cats.push(this.newCat);
+      this.newCat = "";
+      this.saveCats(); //cats配列をlocalStorageに保存
+    },
+    removeCat(cat) {
+      this.cats.splice(cat, 1);
+      this.saveCats(); //再駆除後の配列の状態をlocalStorageに保存
+    },
+    saveCats() {
+      const parsed = JSON.stringify(this.cats); //オブジェクト型のcatsを配列にキャスト
+      localStorage.setItem("cats", parsed);
     },
   },
 });
